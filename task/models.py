34b08a9a1597b8a355faef80953c8802434d191c
FullAkou <fulgencebeugre49@gmail.com>
2025-11-21 18:18:46 +0000

    filtrage des categories par ordre alphabetique

--- task/models.py
+++ task/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+from category.models import Category
+
 # Create your models here.
 class Task(models.Model):
+    category = models.ForeignKey(Category, related_name='tasks', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     is_done = models.BooleanField(default=False)
--- task/views.py
+++ task/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 
 from .models import Task
-
+from category.models import Category
 
 # Create your views here.
 def frontpage(request):
-
     title= "This is a Front Page"
-
     tasks = Task.objects.filter(is_done=True)
+    categories = Category.objects.all()
 
-    return render(request, 'task/frontpage.html', {'title': title, 'tasks': tasks})
+    return render(request, 'task/frontpage.html', {'title': title, 'tasks': tasks, 'categories': categories})

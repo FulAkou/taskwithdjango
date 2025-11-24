bd22c51447449d029caa23a0e3cbf24d8d8e1aaa
FullAkou <fulgencebeugre49@gmail.com>
2025-11-24 12:21:30 +0000

    separating urls

--- toodoo/urls.py
+++ toodoo/urls.py
@@ -1,12 +1,10 @@
 
 
 from django.contrib import admin
-from django.urls import path
-from task.views import frontpage
-from category.views import detail
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', frontpage, name='frontpage'),
-    path('<int:pk>/', detail, name='category_detail'),
+    path('', include('category.urls')),
+    path('', include('task.urls')),
 ]

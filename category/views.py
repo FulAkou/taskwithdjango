41015799f70af9d1a8a8534452e1f1d195b7c69b
FullAkou <fulgencebeugre49@gmail.com>
2025-11-24 11:09:41 +0000

    add category detail page

--- category/views.py
+++ category/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
+def detail(request, pk):
+    category = Category.objects.get(pk=pk)
+    return render(request, 'category/detail.html', {'category': category})

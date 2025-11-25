8242102817fd9f1361605c6b1ad1f0c9ca7e58bb
FullAkou <fulgencebeugre49@gmail.com>
2025-11-25 17:46:17 +0000

    add edit form page

--- task/urls.py
+++ task/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.frontpage, name='frontpage')
+    path('', views.frontpage, name='frontpage'),
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
 ]
--- task/views.py
+++ task/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from category.models import Category
 from .forms import TaskForm
@@ -13,8 +13,21 @@ def frontpage(request):
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            return redirect('frontpage')
     else:
         form = TaskForm()
 
     return render(request, 'task/frontpage.html', {'title': title, 'tasks': tasks, 'categories': categories, 'form': form})
+
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('frontpage')
+    else:
+        form = TaskForm(instance=task)
+
+    return render(request, 'task/edit.html', {'form': form})

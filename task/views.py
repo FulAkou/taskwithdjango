6b2160a51cf7effee5f704e99633e572f013209e
FullAkou <fulgencebeugre49@gmail.com>
2025-11-25 17:14:42 +0000

    add django forms

--- task/views.py
+++ task/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from .models import Task
 from category.models import Category
+from .forms import TaskForm
 
 # Create your views here.
 def frontpage(request):
@@ -9,4 +9,12 @@ def frontpage(request):
     tasks = Task.objects.filter(is_done=True)
     categories = Category.objects.all()
 
-    return render(request, 'task/frontpage.html', {'title': title, 'tasks': tasks, 'categories': categories})
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = TaskForm()
+
+    return render(request, 'task/frontpage.html', {'title': title, 'tasks': tasks, 'categories': categories, 'form': form})

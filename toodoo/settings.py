288c00d9a1c9efe392536830b634ea3b35283da9
FullAkou <fulgencebeugre49@gmail.com>
2025-11-30 22:41:29 +0000

    add sign in functionality

--- toodoo/settings.py
+++ toodoo/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = 'frontpage'
+LOGOUT_REDIRECT_URL = 'signin'
 
 # Application definition
 
--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('signin/', auth_views.LoginView.as_view(template_name='userprofile/signin.html'), name='signin'),
+    # path('signout/', auth_views.LogoutView.as_view(template_name='userprofile/signout.html'), name='signout'),
 ]

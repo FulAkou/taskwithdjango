f8812b8857fc840f2d0feb581e8bb6d571bc0f45
FullAkou <fulgencebeugre49@gmail.com>
2025-11-30 22:26:01 +0000

    add sing-up users

--- toodoo/urls.py
+++ toodoo/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('userprofile.urls')),
     path('', include('category.urls')),
     path('', include('task.urls')),
 ]

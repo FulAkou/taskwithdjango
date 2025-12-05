07e4e62d0d0e4c40a141bd04c8648e2146ac2d47
FullAkou <fulgencebeugre49@gmail.com>
2025-12-05 17:34:36 +0000

    add template filters

--- task/models.py
+++ task/models.py
@@ -9,3 +9,4 @@ class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     is_done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
